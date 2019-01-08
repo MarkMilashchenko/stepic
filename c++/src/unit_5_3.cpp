12e9bfdefe4ad3ae38cf9a9aba6946f638cca672
MarkMilashchenko <dapma@mail.ru>
2019-01-08 17:06:44 +0300

    step_5_3 (#8) (#8)

--- c++/src/unit_5_3.cpp
+++ c++/src/unit_5_3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <tuple>
 #include "unit_5_3.h"
 
 namespace unit_5_3
@@ -7,10 +8,39 @@ namespace unit_5_3
 	{
 		std::cout << unit_5_3::Fib<10>::value << '\n';
 		
-		using primes = unit_5_3::IntList<2,3,5,7,11,13, 12>;
+		using primes = unit_5_3::IntList<2,3,5,7,11,13,12>;
 		constexpr int head = primes::Head;
 		//using odd_primes = primes::Tail;
 		
 		std::cout << "Head: " << head << " Length: " << unit_5_3::Length<primes>::value << '\n';
+		
+		using L1 = unit_5_3::IntList<2,3,4>;
+		using L2 = unit_5_3::IntCons<1, L1>::type;   // IntList<1,2,3,4>
+
+		std::cout << "Head: " << L2::Head << " Length: " << unit_5_3::Length<L2>::value << '\n';
+		using L3 = unit_5_3::Generate<5>::type;      // IntList<0,1,2,3,4>
+		std::cout << "Head: " << L3::Head << " Length: " << unit_5_3::Length<L3>::value << '\n';
+	
+		auto f = [](int x, double y, double z) { return x + y + z; };
+		auto t = std::make_tuple(30, 5.0, 1.6);  // std::tuple<int, double, double>
+		auto res = unit_5_3::apply(f, t);                // res = 36.6
+		std::cout << res << "\n";
+
+		// два списка одной длины
+		using L4 = unit_5_3::IntList<1,2,3,4,5>;
+		using L5 = unit_5_3::IntList<1,3,7,7,2>;
+
+		// результат применени€ Ч список с поэлементными суммами
+		using L6 = unit_5_3::Zip<L4, L5, unit_5_3::Plus>::type;  // IntList<2, 5, 10, 11, 7>
+		std::cout << "Head: " << L6::Head << " Length: " << unit_5_3::Length<L6>::value << '\n';
+
+//		using NumberQ = unit_5_3::Quantity<unit_5_3::Dimension<>>;
+//		using LengthQ = unit_5_3::Quantity<unit_5_3::Dimension<1>>;
+//		using MassQ = unit_5_3::Quantity<unit_5_3::Dimension<0, 1>>;
+//		using TimeQ = unit_5_3::Quantity<unit_5_3::Dimension<0, 0, 1>>;
+//		using VelocityQ = unit_5_3::Quantity<unit_5_3::Dimension<1, 0, -1>>;
+//		using AccelQ = unit_5_3::Quantity<unit_5_3::Dimension<1, 0, -2>>;
+//		using ForceQ = unit_5_3::Quantity<unit_5_3::Dimension<1, 1, -2>>;
+		
 	}
 }
--- c++/src/unit_5_3.h
+++ c++/src/unit_5_3.h
@@ -1,5 +1,7 @@
 //https://stepik.org/lesson/58812/step/5?unit=36393
 #pragma once
+#include <tuple>
+
 
 namespace unit_5_3 {
 	template<int N>
@@ -45,6 +47,77 @@ namespace unit_5_3 {
 	{ 
 		int static const value = 0; 
 	};
+	
+	template<int H, class TL>
+	struct IntCons;
+	
+	template<int H, int ...L>
+	struct IntCons<H, IntList<L...>>
+	{
+		using type = IntList<H, L...>;
+	};
+	
+	template<int H, int B = 0>
+	struct Generate
+	{
+		using type = typename IntCons<B, typename Generate<H - 1, B + 1>::type>::type;
+	};
+	
+	template<int B>
+	struct Generate<0, B>
+	{
+		using type = IntList<>;
+	};
+	
+	template<typename T, typename... Args, int... val>
+	auto calc_fcn(T fcn, std::tuple<Args...> tuple, IntList<val...> list) -> decltype(fcn(std::forward<Args>(std::get<val>(tuple))...))
+	{
+		return fcn(std::forward<Args>(std::get<val>(tuple))...);
+	};
+
+	template<typename T, typename... Args>
+	auto apply(T fcn, std::tuple<Args...> tuple) -> decltype(calc_fcn(fcn, tuple, typename Generate<sizeof... (Args)>::type()))
+	{
+		return calc_fcn(fcn, tuple, typename Generate<sizeof...(Args)>::type());
+	};
+	
+	template<int a, int b>
+	struct Plus
+	{
+    	static int const value = a + b;    
+	};
+	
+	template<typename T1, typename T2, template<int, int> class F>
+	struct Zip;
+
+	template<int... L1, int... L2, template<int, int> class F>
+	struct Zip<IntList<L1...>, IntList<L2...>, F>
+	{
+		static int const value = F<IntList<L1...>::Head, IntList<L2...>::Head>::value;
+        using recursiveTail = typename Zip<typename IntList<L1...>::Tail, typename IntList<L2...>::Tail, F>::type;
+		using type = typename IntCons<value, recursiveTail>::type;
+	};
+	
+	template<template<int, int> class F>
+	struct Zip<IntList<>, IntList<>, F>
+	{
+		using type = IntList<>;
+	};
+	
+	
+//	template<int m = 0, int kg = 0, int s = 0, int A = 0, int K = 0, int mol = 0, int cd = 0>
+//	using Dimension = IntList<m, kg, s, A, K, mol, cd>;
+//	
+//	template<IntList<int... Args> list>
+//	struct Quantity
+//	{
+//		Quantity();
+//		explicit Quantity(double input);
+//		
+//		void operator+();
+//		
+//		double value;
+//	};
 	
 	void test();
 }
